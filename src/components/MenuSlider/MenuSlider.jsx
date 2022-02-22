ac55145ab79f8c46786cda02db3db2de72f23f58
Matias Pedelhez <matiaspedelhezinf@gmail.com>
2022-02-21 22:41:48 -0300

    Added slider

--- src/components/MenuSlider/MenuSlider.jsx
+++ src/components/MenuSlider/MenuSlider.jsx
@@ -1,21 +1,48 @@
 import React, { useRef, useEffect, useState } from "react";
-import motion from "framer-motion";
+import { motion } from "framer-motion";
 import "./MenuSlider.scss";
 import coffee_items from "../../coffee_items";
 import MenuSliderCard from "../MenuSliderCard/MenuSliderCard";
 
 function MenuSlider() {
+  // const [innerCarouselWidth, setInnerCarouselWidth] = useState(0);
+  const innerCarouselRef = useRef(null);
+
+  // useEffect(() => {
+  //   function handleInnerCarouselWidth() {
+  //     setInnerCarouselWidth(
+  //       innerCarouselRef.current.offsetWidth -
+  //         innerCarouselRef.current.scrollWidth
+  //     );
+  //   }
+  //   window.addEventListener("resize", handleInnerCarouselWidth);
+  //   handleInnerCarouselWidth();
+
+  //   return (_) => {
+  //     window.removeEventListener("resize", handleInnerCarouselWidth);
+  //   };
+  // });
+
   return (
     <div className="MenuSlider">
-      {coffee_items.map((item) => {
-        return (
-          <MenuSliderCard
-            card_name={item.card_name}
-            card_description={item.card_description}
-            key={item.id}
-          />
-        );
-      })}
+      <motion.div className="carousel" whileTap={{ cursor: "grabbing" }}>
+        <motion.div
+          ref={innerCarouselRef}
+          drag="x"
+          dragConstraints={innerCarouselRef}
+          className="inner-carousel"
+        >
+          {coffee_items.map((item) => {
+            return (
+              <MenuSliderCard
+                card_name={item.card_name}
+                card_description={item.card_description}
+                key={item.id}
+              />
+            );
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
--- src/components/MenuSliderCard/MenuSliderCard.jsx
+++ src/components/MenuSliderCard/MenuSliderCard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./MenuSliderCard.scss";
+import { motion } from "framer-motion";
 
 function MenuSliderCard({ card_name, card_description }) {
   return (
-    <div className="MenuSliderCard">
+    <motion.div className="MenuSliderCard">
       <h2 className="menu-card-title">{card_name}</h2>
       <p className="menu-card-description">{card_description}</p>
       <p className="menu-card-button">Order Now</p>
-    </div>
+    </motion.div>
   );
 }
 
